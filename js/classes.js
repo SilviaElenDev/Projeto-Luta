5473dc7cb5e8ffc5ae8225064f837aa730de5588
Silvia <silviaelen.developer@gmail.com>
2024-12-04 15:43:36 -0300

    Concluindo Projeto

--- js/classes.js
+++ js/classes.js
@@ -66,14 +66,70 @@ class BigMonster extends Character {
 //Classes Cenario
 
 class Stage {
-    constructor(fighter1, fighter2, fighter1El, fighter2El){
+    constructor(fighter1, fighter2, fighter1El, fighter2El,logObject){
         this.fighter1 = fighter1;
         this.fighter2 = fighter2;
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
+        this.log = logObject;
+    }
+
+    start(){
+        this.update();
+
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1,this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2,this.fighter1));
+    }
+
+    update(){
+        this.fighter1El.querySelector('.name').innerHTML = ` ${this.fighter1.name} - ${this.fighter1.life.toFixed(1)}HP `;
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`;
+
+        this.fighter2El.querySelector('.name').innerHTML = ` ${this.fighter2.name} - ${this.fighter2.life.toFixed(1)}HP `;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`;
+    }
+
+    doAttack(attackin, attacked){
+        if(attackin.life <= 0 || attacked.life <=0){
+            this.log.addMessage('Atacando cachorro morto');
+            return
+        }
+
+        let attackFactor = (Math.random()* 2).toFixed(2);
+        let defenseFactor = (Math.random()* 2).toFixed(2);
+
+        let actualDefense = attacked.defense * defenseFactor;
+        let actualAttack =  attackin.attack * attackFactor;
+
+        if (actualAttack > actualDefense){
+            attacked.life -= actualAttack;
+            this.log.addMessage(`${attackin.name} causou ${actualAttack.toFixed(1)} de dano em ${attacked.name}`);
+        }else{
+            this.log.addMessage(`${attacked.name} conseguiu se defender`)
+        }
+        this.update()
     }
 }
 
-start(){
-    this.update()
+class Log {
+    list = [];
+
+    constructor(listEl){
+        this.listEl = listEl;
+    }
+
+    addMessage(msg){
+        this.list.push(msg);
+        this.render();
+    }
+
+    render(){
+        this.listEl.innerHTML = "";
+
+        for(let i in this.list){
+            this.listEl.innerHTML += `<li>${this.list[i]}</li>` ;
+        }
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
+let log = new Log(document.querySelector('.log'));
+
 let char = new Kinght('Silvia');
-let monster = new LitteMonster();
+let monster = new Sorcerer('João');
+
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster'),
+    log
+);
+
+stage.start();
+
